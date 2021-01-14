417677202b45fd10274d09b3d1d3d4b961948211
Kin-Kossakow <73644765+Kin-Kossakow@users.noreply.github.com>
2021-01-14 12:34:35 +0100

    Update zad1.hpp

--- include/zad1.hpp
+++ include/zad1.hpp
@@ -1,5 +1,30 @@
 #include "catch.hpp"
 
+
+class Tagliatelle
+{
+private:
+    double              L;
+    double              W;
+    double              R;
+    const static double C;
+
+public:
+    Tagliatelle(double l, double w, double r)
+    {
+        L = l;
+        W = w;
+        R = r;
+    }
+    Tagliatelle()
+    {
+        L = 0.5;
+        W = 0.5;
+        R = 0.5;
+    }
+
+    double ileMaki(unsigned P) const { return P * L * W * (1. - R) * C; }
+};
 // Zad2
 // tutaj definicja klasy Makaron
 // BEZ definicji metody gotujMakaron (tylko z deklaracją)

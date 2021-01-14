70c56ad07bb9ad9857a253c93cca5ffcfb4d8ba9
Kin-Kossakow <73644765+Kin-Kossakow@users.noreply.github.com>
2021-01-14 12:45:11 +0100

    Update zad2.hpp

--- include/zad2.hpp
+++ include/zad2.hpp
@@ -5,5 +5,23 @@
 #include <type_traits>
 
 // tutaj klasa Penne
+class Penne : public Makaron
+{
+public:
+    double ileMaki(unsigned P) const { return P; } 
+
+};
 
 // tutaj definicja metody gotujMakaron
+
+inline Makaron* Makaron::gotujMakaron(const std::string& name)
+{
+    if (name.front() == name.back())
+    {
+        return new Tagliatelle(3.14, 0.42, 0.1);
+    }
+    else
+    {
+        return new Penne();
+    }
+}

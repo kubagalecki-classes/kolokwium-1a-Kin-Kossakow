ab5487d04675bbfe5c2e50a941a4959be68d714e
Kin-Kossakow <73644765+Kin-Kossakow@users.noreply.github.com>
2021-01-14 13:04:37 +0100

    Update zad4.hpp

--- include/zad4.hpp
+++ include/zad4.hpp
@@ -3,3 +3,26 @@
 #include <vector>
 
 // tutaj funkcja obliczMake
+
+double obliczMake(std::vector<Tagliatelle> vector)
+{
+    double M = 0;
+    unsigned multiplier = 1;
+    for (size_t i = vector.size() - 1; i >= 0; --i)
+    {
+        M += vector[i].ileMaki(multiplier);
+        multiplier++;
+    }
+
+    if (M > 100)
+    {
+        throw int(10);
+    }
+
+    if (M > 50 && M <= 100)
+    {
+        throw double(234);
+    }
+
+    return M;
+}
